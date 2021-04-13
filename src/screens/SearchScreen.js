d8aa46a9a97bb390a446d6ba3f2aac54977ccc27
Dylan Cooper <dylancooper155@gmail.com>
2021-04-13 16:22:45 -0400

    adjusting searchApi

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,8 +6,10 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
+    try {
        const response = await yelp.get('/search', {
            params: {
                limit: 50,
@@ -16,13 +18,16 @@ const SearchScreen = () => {
            }
        });
        setResults(response.data.businesses);
+    } catch (err) {
+        setErrorMessage('Something went wrong');
+    }
     };
 
     return <View>
         <SearchBar term={term} onTermChange={setTerm} 
         onTermSubmit={searchApi}
         />
-        <Text>Search Screen</Text>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results</Text>
     </View>
 };
